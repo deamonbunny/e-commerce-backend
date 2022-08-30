add9f5c7bf26bb1f1a1b2d8f5d12556c5024b465
bunny <deamonbunny@gmail.com>
2022-08-30 00:25:39 -0700

    filled api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,21 +15,81 @@ router.get('/', (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model:Product}]
+    });
+  if (!categoryData) {
+    res.status(404).json({
+      msg: "Category ID not Found"
+    });
+  }
+  res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const newCategory = await Category.create({
+      category_name:req.body.category_name,
+    })
+    res.status(201).json(newCategory)
+  }catch(err){
+    console.log(err)
+    res.status(500).json({
+      msg:"Server Error", err
+    })
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name:req.body.category_name
+  },
+      {
+      where:{
+          id:req.params.id
+      }
+      }).then(category=>{
+          if(!category[0]){
+              return res.status(404).json({
+                msg:"Category Does Not Exist or No Change Was Made"
+              })
+          }
+      res.json(category)
+  }).catch(err=>{
+      res.status(500).json({
+        msg:"Server Error",err
+      })
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where:{
+      id:req.params.id
+    }
+  }).then(category=>{
+    if(!category){
+      return res.status(404).json({
+        msg:"Category Does Not Exist"
+      })
+    }
+    res.json(category)
+  }).catch(err=>{
+    res.status(500).json({
+      msg:"internal server error",err
+    })
+  })
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,92 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll(({
+    include: [{model: Product}]
+  })).then(data=>{
+    res.json(data)
+  }).catch(err=>{
+    res.status(500).json({
+      msg: "Server Error", err
+    })
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try { 
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model:Product}]
+    });
+  if (!tagData) {
+    res.status(404).json({
+      msg: "Product ID not Found"
+    });
+    return;
+  }
+  res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(201).json(newTag)
+  }catch(err){
+    console.log(err)
+    res.status(500).json({
+      msg:"Server Error", err
+    })
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name:req.body.tag_name
+  },
+    {
+      where:{
+        id:req.params.id
+      }
+    }).then(tag=>{
+      if(!tag[0]){
+        return res.status(404).json({
+          msg:"Tag Not Found or No Changes Made"
+        })
+      }
+      res.json(tag)
+    }).catch(err=>{
+      res.status(500).json({
+        msg:"Server Error", err
+      })
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id:req.params.id
+    }
+  }).then(tag=>{
+    if(!tag){
+      return res.status(404).json({
+        msg: "Tag ID not Found"
+      })
+    }
+    res.json(tag)
+  }).catch(err=>{
+    res.status(500).json({
+      msg:"Server Error", err
+    })
+  })
 });
 
 module.exports = router;
